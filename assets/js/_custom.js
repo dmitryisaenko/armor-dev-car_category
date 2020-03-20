0de678f1fcc8e70f5689820ecfc38ae1f30475f5
Dmitry Isaenko <isaenkodmitry@gmail.com>
2020-03-20 14:24:10 +0200

    Html-layout done. Content and Articles adaptive. Working with header (promo) part as adaptive

--- assets/js/_custom.js
+++ assets/js/_custom.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	
+	var $filter = $('.archivepage-filter__title');
+       
+   
+   $filter.on('click', function() {
+      $(this).siblings('.archivepage-filter__items').toggleClass('filter-items_hide');
+      $(this).find('.archivepage-filter__icon').toggleClass('archivepage-filter__icon_closed');
+   });
 
 });
 
